3ab791f2e44fc39a30680b39ac4d5ffa33f7c1dc
EmrahMorgil <morgilemrah@gmail.com>
2023-11-26 12:36:47 +0300

    settings ve online barı için animasyon eklendi.

--- chatapp-client/src/components/User/UserBar/UserBar.tsx
+++ chatapp-client/src/components/User/UserBar/UserBar.tsx
@@ -12,10 +12,12 @@ const UserBar = () => {
     <div style={{ backgroundColor: "#202C33", height: "70px", width: "100%", display: "flex", alignItems: "center", justifyContent: "space-between" }}>
       <div>
         <img style={{ borderRadius: "50%", marginLeft: "30px" }} alt='' src={activeUserParse.image} width={"50px"} height={"50px"} />
+        <div className={"ms-3 online-point-header"}></div>
+        <span style={{color: "white"}} className='ms-3'>Online</span>
       </div>
       <div className="dropdown" style={{ marginRight: "10px" }}>
-        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-          ⚙️
+        <button className="btn btn-secondary dropdown-toggle"id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+        <span className="rotate" aria-hidden="true">⚙️</span>
         </button>
         <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
           <li><button className="dropdown-item btn btn-danger" onClick={HandleLogout}>Logout</button></li>
